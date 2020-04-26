750d8b88cac02eea23dd36ce187498a68748c002
Sean Rich <srich366@gmail.com>
2020-04-26 03:10:35 -0400

    Increase icon size on focus

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,18 +5,25 @@
     clear-icon="mdi-close"
     type="text"
     solo
+    @focus="focused = true"
+    @blur="focused = false"
   >
     <template v-slot:prepend-inner>
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
-          <v-icon v-on="on">mdi-magnify</v-icon>
+          <v-icon
+            v-on="on"
+            :color="focused ? 'blue' : undefined"
+            :large="focused"
+            >mdi-magnify</v-icon
+          >
         </template>
         Search for link
       </v-tooltip>
       <v-divider vertical class="mr-2 pl-2"></v-divider>
     </template>
     <template v-slot:append>
-      <v-icon @click="clearSearch">mdi-close</v-icon>
+      <v-icon @click="clearSearch" :large="focused">mdi-close</v-icon>
     </template>
   </v-text-field>
 </template>
@@ -26,7 +33,8 @@ export default {
   name: "SearchBar",
   data() {
     return {
-      search: ""
+      search: "",
+      focused: false
     };
   },
   methods: {
